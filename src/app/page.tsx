1ee7254fe94779569034c529560f19d034fc82a2
mory-jin <adhisurya243@gmail.com>
2026-02-02 20:19:29 +0800

    UI: fix social media sidebar position, tooltip left, and sizing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -119,6 +119,86 @@ export default function Home() {
               </button>
             </div>
           </div>
+          
+          {/*Media sosial*/}
+          <div className="fixed right-6 top-1/2 -translate-y-1/2 z-50 ">
+              {/* <h3 className="text-2xl font-bold mb-6 bg-gradient-to-r from-purple-400 to-blue-400 bg-clip-text text-transparent">
+                Follow Us
+              </h3>
+              <p className="text-gray-400 mb-8">Ikuti kami di social media untuk update terbaru!</p> */}
+              <br/>
+              <div className="flex flex-col items-center gap-7">
+                {/* Instagram */}
+                <a 
+                  href="https://www.instagram.com/skymoon_net/?hl=id" 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                  className="group relative"
+                >
+                  <div className="w-16 h-16 bg-gradient-to-br from-purple-600 via-pink-600 to-orange-600 rounded-full flex items-center justify-center transform transition-all duration-300 group-hover:scale-110 group-hover:shadow-lg group-hover:shadow-purple-500/50">
+                    <svg className="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 24 24">
+                      <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zM5.838 12a6.162 6.162 0 1112.324 0 6.162 6.162 0 01-12.324 0zM12 16a4 4 0 110-8 4 4 0 010 8zm4.965-10.405a1.44 1.44 0 112.881.001 1.44 1.44 0 01-2.881-.001z"/>
+                    </svg>
+                  </div>
+                  <span className="absolute -bottom-6 left-1/2 transform -translate-x-1/2 text-xs text-purple-400 opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap">
+                    Instagram
+                  </span>
+                </a>
+
+                {/* Facebook */}
+                <a 
+                  href="#" 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                  className="group relative"
+                >
+                  <div className="w-16 h-16 bg-gradient-to-br from-blue-600 to-blue-800 rounded-full flex items-center justify-center transform transition-all duration-300 group-hover:scale-110 group-hover:shadow-lg group-hover:shadow-blue-500/50">
+                    <svg className="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 24 24">
+                      <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
+                    </svg>
+                  </div>
+                  <span className="absolute -bottom-6 left-1/2 transform -translate-x-1/2 text-xs text-blue-400 opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap">
+                    Facebook
+                  </span>
+                </a>
+
+                {/* TikTok */}
+                <a 
+                  href="https://www.tiktok.com/@skymoon_nett?_r=1&_t=ZS-917HJwFJMxR" 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                  className="group relative"
+                >
+                  <div className="w-16 h-16 bg-gradient-to-br from-black via-gray-800 to-black rounded-full flex items-center justify-center transform transition-all duration-300 group-hover:scale-110 group-hover:shadow-lg group-hover:shadow-gray-500/50 border border-gray-600">
+                    <svg className="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 24 24">
+                      <path d="M12.525.02c1.31-.02 2.61-.01 3.91-.02.08 1.53.63 3.09 1.75 4.17 1.12 1.11 2.7 1.62 4.24 1.79v4.03c-1.44-.05-2.89-.35-4.2-.97-.57-.26-1.1-.59-1.62-.93-.01 2.92.01 5.84-.02 8.75-.08 1.4-.54 2.79-1.35 3.94-1.31 1.92-3.58 3.17-5.91 3.21-1.43.08-2.86-.31-4.08-1.03-2.02-1.19-3.44-3.37-3.65-5.71-.02-.5-.03-1-.01-1.49.18-1.9 1.12-3.72 2.58-4.96 1.66-1.44 3.98-2.13 6.15-1.72.02 1.48-.04 2.96-.04 4.44-.99-.32-2.15-.23-3.02.37-.63.41-1.11 1.04-1.36 1.75-.21.51-.15 1.07-.14 1.61.24 1.64 1.82 3.02 3.5 2.87 1.12-.01 2.19-.66 2.77-1.61.19-.33.4-.67.41-1.06.1-1.79.06-3.57.07-5.36.01-4.03-.01-8.05.02-12.07z"/>
+                    </svg>
+                  </div>
+                  <span className="absolute -bottom-6 left-1/2 transform -translate-x-1/2 text-xs text-gray-400 opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap">
+                    TikTok
+                  </span>
+                </a>
+                {/* Discord */}
+                <a 
+                  href="https://discord.gg/58kmgW4sW8" 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                  className="group relative"
+                >
+                  <div className="w-16 h-16 bg-gradient-to-br from-[#5865F2] via-[#4752C4] to-[#404EED] rounded-full flex items-center justify-center  transform transition-all duration-300 group-hover:scale-110 group-hover:shadow-lg group-hover:shadow-[#5865F2]/50 border border-[#5865F2]">
+
+                    <svg
+                      className="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                      <path d="M20.317 4.369a19.791 19.791 0 00-4.885-1.515.074.074 0 00-.079.037c-.211.375-.444.864-.608 1.249a18.27 18.27 0 00-5.487 0 12.64 12.64 0 00-.617-1.249.077.077 0 00-.079-.037 19.736 19.736 0 00-4.885 1.515.07.07 0 00-.032.027C.533 9.045-.32 13.58.099 18.057a.082.082 0 00.031.056 19.9 19.9 0 006.012 3.03.077.077 0 00.084-.027c.462-.63.873-1.295 1.226-1.994a.076.076 0 00-.041-.106 13.201 13.201 0 01-1.872-.9.077.077 0 01-.008-.127c.126-.095.252-.194.372-.291a.074.074 0 01.077-.01c3.927 1.793 8.18 1.793 12.061 0a.073.073 0 01.078.009c.12.098.246.197.372.292a.077.077 0 01-.006.127 12.299 12.299 0 01-1.873.899.077.077 0 00-.04.107c.36.698.772 1.364 1.225 1.994a.076.076 0 00.084.028 19.86 19.86 0 006.012-3.03.077.077 0 00.032-.055c.5-5.177-.838-9.673-3.548-13.66a.061.061 0 00-.031-.028zM8.02 15.331c-1.183 0-2.156-1.085-2.156-2.419 0-1.333.955-2.418 2.157-2.418 1.21 0 2.175 1.095 2.156 2.418 0 1.334-.955 2.419-2.156 2.419zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.955-2.418 2.157-2.418 1.21 0 2.175 1.095 2.156 2.418 0 1.334-.946 2.419-2.156 2.419z"/>
+                    </svg>
+
+                  </div>
+                  <span className="absolute -bottom-6 left-1/2 transform -translate-x-1/2 text-xs text-gray-400 opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap">
+                    Discord
+                  </span>
+                </a>
+              </div>
+            </div>
 
           {/* Scroll Indicator
           <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
@@ -369,6 +449,25 @@ export default function Home() {
                   </div>
                   <span className="absolute -bottom-6 left-1/2 transform -translate-x-1/2 text-xs text-gray-400 opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap">
                     TikTok
+                  </span>
+                </a>
+                {/* Discord */}
+                <a 
+                  href="https://discord.gg/58kmgW4sW8" 
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                  className="group relative"
+                >
+                  <div className="w-16 h-16 bg-gradient-to-br from-[#5865F2] via-[#4752C4] to-[#404EED] rounded-full flex items-center justify-center  transform transition-all duration-300 group-hover:scale-110 group-hover:shadow-lg group-hover:shadow-[#5865F2]/50 border border-[#5865F2]">
+
+                    <svg
+                      className="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                      <path d="M20.317 4.369a19.791 19.791 0 00-4.885-1.515.074.074 0 00-.079.037c-.211.375-.444.864-.608 1.249a18.27 18.27 0 00-5.487 0 12.64 12.64 0 00-.617-1.249.077.077 0 00-.079-.037 19.736 19.736 0 00-4.885 1.515.07.07 0 00-.032.027C.533 9.045-.32 13.58.099 18.057a.082.082 0 00.031.056 19.9 19.9 0 006.012 3.03.077.077 0 00.084-.027c.462-.63.873-1.295 1.226-1.994a.076.076 0 00-.041-.106 13.201 13.201 0 01-1.872-.9.077.077 0 01-.008-.127c.126-.095.252-.194.372-.291a.074.074 0 01.077-.01c3.927 1.793 8.18 1.793 12.061 0a.073.073 0 01.078.009c.12.098.246.197.372.292a.077.077 0 01-.006.127 12.299 12.299 0 01-1.873.899.077.077 0 00-.04.107c.36.698.772 1.364 1.225 1.994a.076.076 0 00.084.028 19.86 19.86 0 006.012-3.03.077.077 0 00.032-.055c.5-5.177-.838-9.673-3.548-13.66a.061.061 0 00-.031-.028zM8.02 15.331c-1.183 0-2.156-1.085-2.156-2.419 0-1.333.955-2.418 2.157-2.418 1.21 0 2.175 1.095 2.156 2.418 0 1.334-.955 2.419-2.156 2.419zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.955-2.418 2.157-2.418 1.21 0 2.175 1.095 2.156 2.418 0 1.334-.946 2.419-2.156 2.419z"/>
+                    </svg>
+
+                  </div>
+                  <span className="absolute -bottom-6 left-1/2 transform -translate-x-1/2 text-xs text-gray-400 opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap">
+                    Discord
                   </span>
                 </a>
               </div>
